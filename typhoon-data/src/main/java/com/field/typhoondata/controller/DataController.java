585699b19427d902163b30fda2e54376dec815ef
field <549427031@qq.com>
2019-04-23 15:40:28 +0800

    feat:整合feign

---
+++ typhoon-data/src/main/java/com/field/typhoondata/controller/DataController.java
@@ -0,0 +1,7 @@
+package com.field.typhoondata.controller;
+
+/**
+ * Created by Tian.Ye in 15:15 2019/4/23
+ */
+public class DataController {
+}
---
+++ typhoon-query/src/main/java/com/field/typhoonquery/service/DataFeignService.java
@@ -0,0 +1,7 @@
+package com.field.typhoonquery.service;
+
+/**
+ * Created by Tian.Ye in 15:13 2019/4/23
+ */
+public class DataFeignService {
+}
