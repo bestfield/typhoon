aa88c8ab9f66c01e776675b34e3003a97aaf604f
field <549427031@qq.com>
2019-04-28 11:17:40 +0800

    feat:集成监控中心

---
+++ typhoon-admin-server/src/test/java/com/field/typhoonadminserver/TyphoonAdminServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.field.typhoonadminserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TyphoonAdminServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
